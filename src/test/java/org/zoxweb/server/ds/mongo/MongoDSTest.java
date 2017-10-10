418778817cae35829f80dc725277aade5a1dbadc
javaconsigliere <javaconsigliere@gmail.com>
2017-10-10 09:58:08 -0700

    update

--- src/test/java/org/zoxweb/server/ds/mongo/MongoDSTest.java
+++ src/test/java/org/zoxweb/server/ds/mongo/MongoDSTest.java
@@ -4,9 +4,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyStore;
-
+import java.util.Collection;
 import java.util.logging.Logger;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.RealmSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.zoxweb.server.io.IOUtil;
 import org.zoxweb.server.security.CryptoUtil;
 import org.zoxweb.server.security.KeyMakerProvider;
 import org.zoxweb.server.security.shiro.DefaultAPISecurityManager;
@@ -15,6 +25,7 @@ import org.zoxweb.server.util.GSONUtil;
 import org.zoxweb.shared.api.APIConfigInfoDAO;
 import org.zoxweb.shared.api.APIDataStore;
 import org.zoxweb.shared.data.ApplicationConfigDAO;
+import org.zoxweb.shared.data.StatCounter;
 
 public class MongoDSTest 
 {
@@ -67,6 +78,66 @@ public class MongoDSTest
 			// create the data store
 			dsConfig.setKeyMaker(KeyMakerProvider.SINGLETON);
 			dsConfig.setAPISecurityManager(new DefaultAPISecurityManager());
+			
+			
+			
+			
+			
+			int index = 0;
+			ClassLoader classLoader = MongoDSTest.class.getClassLoader();
+			String filename = args[index++];
+			File file = new File(classLoader.getResource(filename).getFile());
+			System.out.println(IOUtil.inputStreamToString(file));
+	
+			//1.
+		    Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:" + filename);
+
+		    //2.
+		    SecurityManager securityManager = factory.getInstance();
+
+		    //3.
+		    SecurityUtils.setSecurityManager(securityManager);
+		    Subject currentUser = SecurityUtils.getSubject();
+		    Session session = currentUser.getSession();
+		    session.setAttribute( "someKey", "aValue" );
+
+		    String subjectID = args[index++];
+		    String password  = args[index++];
+		    if ( !currentUser.isAuthenticated() ) {
+		        //collect user principals and credentials in a gui specific manner
+		        //such as username/password html form, X509 certificate, OpenID, etc.
+		        //We'll use the username/password example here since it is the most common.
+		        UsernamePasswordToken token = new UsernamePasswordToken(subjectID, password);
+
+		        //this is all you have to do to support 'remember me' (no config - built in!):
+		        token.setRememberMe(true);
+
+		        currentUser.login(token);
+		    }
+		    
+		    
+		    Collection<Realm> realms =  ((RealmSecurityManager)SecurityUtils.getSecurityManager()).getRealms();
+		    System.out.println(realms);
+		    
+		    String permissions[]=
+		    	{
+		    		"read:batata",
+		    		"write:batata", 
+		    		"update:batata",
+		    		"batata:update",
+		    		"batata:update:all"
+		    	};
+		    
+		    StatCounter sc = new StatCounter();
+		    for(int i=0 ; i < 1; i++)
+		    for (String permission : permissions)
+		    {
+		    		//currentUser.isPermitted(permission);
+		    		
+		    		System.out.println(permission + " stat:" + currentUser.isPermitted(permission));
+		    }
+		    System.out.println(sc.deltaSinceCreation());
+			
 			
 			
 			
