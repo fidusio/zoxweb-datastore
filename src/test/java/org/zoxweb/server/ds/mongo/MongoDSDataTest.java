6e2d4d5d1115e2a1ac1b0c3bc9cdea21d74c7e25
javaconsigliere <javaconsigliere@gmail.com>
2018-08-23 11:47:08 -0700

    Added test case for EncryptedKey DAO

--- src/test/java/org/zoxweb/server/ds/mongo/MongoDSDataTest.java
+++ src/test/java/org/zoxweb/server/ds/mongo/MongoDSDataTest.java
@@ -21,6 +21,8 @@ import org.zoxweb.shared.api.APIAppManager;
 import org.zoxweb.shared.api.APIConfigInfoDAO;
 import org.zoxweb.shared.api.APIDataStore;
 import org.zoxweb.shared.api.APISecurityManager;
+import org.zoxweb.shared.crypto.EncryptedDAO;
+import org.zoxweb.shared.crypto.EncryptedKeyDAO;
 import org.zoxweb.shared.data.AppIDDAO;
 import org.zoxweb.shared.db.QueryMatchString;
 import org.zoxweb.shared.security.JWT;
@@ -32,28 +34,34 @@ import org.zoxweb.shared.util.Const.RelationalOperator;
 import org.zoxweb.shared.util.NVBigDecimal;
 import org.zoxweb.shared.util.NVBlob;
 import org.zoxweb.shared.util.NVBoolean;
+import org.zoxweb.shared.util.NVConfigEntity;
 import org.zoxweb.shared.util.NVDouble;
 import org.zoxweb.shared.util.NVEnum;
 import org.zoxweb.shared.util.NVFloat;
 import org.zoxweb.shared.util.NVGenericMap;
 import org.zoxweb.shared.util.NVInt;
 import org.zoxweb.shared.util.NVLong;
+import org.zoxweb.shared.util.NVPair;
 import org.zoxweb.shared.util.NVStringList;
 import org.zoxweb.shared.util.ResourceManager;
 import org.zoxweb.shared.util.ResourceManager.Resource;
-
+import org.zoxweb.shared.util.SharedStringUtil;
 import java.io.FileInputStream;
 
 import java.io.IOException;
 
 import java.math.BigDecimal;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.List;
-
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -202,6 +210,22 @@ public class MongoDSDataTest {
     }
     
     @Test
+    public void testEncryptedKeyDOA() throws InvalidKeyException, NullPointerException, IllegalArgumentException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException
+    {
+      EncryptedDAO ed = CryptoUtil.encryptDAO(new EncryptedKeyDAO(), SharedStringUtil.getBytes("PASSWORD"), null, 1);
+      ed.getSubjectProperties().add(new NVPair("mario", "taza"));
+      ed.getAlgoProperties().add(new NVPair("taza", "mario"));
+      System.out.println(GSONUtil.toJSON(ed, false));
+      ed = dataStore.insert(ed);
+      System.out.println(GSONUtil.toJSON(ed, false));
+      System.out.println(ed.getNVConfig().getName() + "," + ed.getReferenceID());
+      List<EncryptedKeyDAO> l = dataStore.searchByID((NVConfigEntity)ed.getNVConfig(), ed.getReferenceID());
+      assert(l != null && l.size() == 1 );
+      System.out.println(GSONUtil.toJSON(l.get(0), false));
+      
+    }
+    
+    //@Test
     public void testReadNVGM() throws IOException
     {
     	List<JWT> results = dataStore.search(JWT.NVC_JWT, null, new QueryMatchString(RelationalOperator.EQUAL, "jwtHS256", "name" ));
